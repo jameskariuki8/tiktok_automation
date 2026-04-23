9fa608333a364b8c2799eea80e23efe3c06d2311
jameskariuki8 <juniorkariuki735@gmail.com>
2026-04-23 15:27:11 +0300

    first commit

--- apps/tiktok/urls.py
+++ apps/tiktok/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from .views import TikTokAuthUrlView, TikTokCallbackView, TikTokWebhookView, TikTokAccountListView, TikTokLoginRedirectView, TikTokDisconnectView
 
 urlpatterns = [
