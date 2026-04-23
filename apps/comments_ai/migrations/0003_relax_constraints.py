fe922d8f574ffc2f64442ccbd9de94b076c9b8d6
jameskariuki8 <juniorkariuki735@gmail.com>
2026-04-23 21:32:59 +0300

    first commit

---
+++ apps/comments_ai/migrations/0003_relax_constraints.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('comments_ai', '0002_knowledgedocument'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='commentsuggestion',
+            name='tiktok_video_id',
+            field=models.CharField(blank=True, max_length=255, null=True),
+        ),
+        migrations.AlterField(
+            model_name='commentsuggestion',
+            name='comment_id',
+            field=models.CharField(blank=True, max_length=255, null=True),
+        ),
+        migrations.AlterField(
+            model_name='commentsuggestion',
+            name='commenter_username',
+            field=models.CharField(blank=True, max_length=255, null=True),
+        ),
+    ]
