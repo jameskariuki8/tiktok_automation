685e8c0c0181c699718644e4d7f266336c3fc559
jameskariuki8 <juniorkariuki735@gmail.com>
2026-04-24 21:24:43 +0300

    first commit

--- apps/analytics/urls.py
+++ apps/analytics/urls.py
@@ -3,8 +3,11 @@ from rest_framework.routers import DefaultRouter
 from .views import AnalyticsViewSet
 
 router = DefaultRouter()
-router.register(r'', AnalyticsViewSet, basename='analytics')
+router.register(r'dash', AnalyticsViewSet, basename='analytics')
 
 urlpatterns = [
+    path('inbox/', AnalyticsViewSet.as_view({'get': 'inbox'})),
+    path('video_list/', AnalyticsViewSet.as_view({'get': 'video_list'})),
+    path('post_reply/', AnalyticsViewSet.as_view({'post': 'post_reply'})),
     path('', include(router.urls)),
 ]
